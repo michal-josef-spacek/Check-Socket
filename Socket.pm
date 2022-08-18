865627e993a57db3718d3980fd4fc0f33a58bb17
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-08-18 13:56:05 +0200

    Prepare for 0.02 version

--- Socket.pm
+++ Socket.pm
@@ -11,7 +11,7 @@ use Readonly;
 our $ERROR_MESSAGE;
 Readonly::Array our @EXPORT_OK => qw(check_socket $ERROR_MESSAGE);
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 sub check_socket {
 	my ($config_hr, $os, $env_hr) = @_;
