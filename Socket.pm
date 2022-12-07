711d8cffbaf480db4a236518e1ff53c4fdb5c7e0
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-12-08 00:03:23 +0100

    Prepare for 0.04 version

--- Socket.pm
+++ Socket.pm
@@ -11,7 +11,7 @@ use Socket;
 our $ERROR_MESSAGE;
 Readonly::Array our @EXPORT_OK => qw(check_socket $ERROR_MESSAGE);
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 sub check_socket {
 	my ($config_hr, $os, $env_hr) = @_;
@@ -188,6 +188,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
